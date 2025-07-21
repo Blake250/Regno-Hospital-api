4d9ecb168a6d52adbc34cf0e07f62cc2150b45c6
Blake250 <ozoekwecelestine@gmail.com>
2025-07-21 21:46:49 +0100

    pushing stripe fix

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,19 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);      
+
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);          
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    stripe
+}
