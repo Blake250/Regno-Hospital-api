7a89b6f8fe02a1b1dc8ea8e9990810a864357a45
Blake250 <ozoekwecelestine@gmail.com>
2025-07-27 21:07:57 +0100

    pushing to github

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect, adminOnly } = require('../middleWares/authMiddleWare');
-const {  addDoctor, addDocPhoto, appointmentAdmin, appointmentCancel, getAllDoctors } = require('../controllers/adminController');
+const {  addDoctor, addDocPhoto, appointmentAdmin, appointmentCancel,  } = require('../controllers/adminController');
 
-router.get('/get-docs', protect, getAllDoctors);
+// router.get('/get-docs', protect, getAllDoctors);
 router.post('/add-doc', protect, adminOnly, addDoctor);
 router.post('/add-doc-photo', protect, adminOnly, addDocPhoto);
 router.get('/appointments', protect, adminOnly, appointmentAdmin);
