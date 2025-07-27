52832c40f98de25829ef1fc07701adbef82c684f
Blake250 <ozoekwecelestine@gmail.com>
2025-07-27 17:00:27 +0100

    fixing admin error

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { protect, adminOnly } = require('../middleWares/authMiddleWare');
 const {  addDoctor, addDocPhoto, appointmentAdmin, appointmentCancel } = require('../controllers/adminController');
 
-//router.get('/get-docs', protect, getAllDoctors);
+router.get('/get-docs', protect, getAllDoctors);
 router.post('/add-doc', protect, adminOnly, addDoctor);
 router.post('/add-doc-photo', protect, adminOnly, addDocPhoto);
 router.get('/appointments', protect, adminOnly, appointmentAdmin);
@@ -12,6 +12,6 @@ router.get('/cancelled-appointments', protect, adminOnly, appointmentCancel);
 
 module.exports = router;
 
-
+ 
 
 
