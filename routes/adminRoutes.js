63a6ab730eaab1299dbdae5ee6d329e5a3f41af6
Blake250 <ozoekwecelestine@gmail.com>
2025-07-27 15:58:33 +0100

    pushing getDocs fix

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect, adminOnly } = require('../middleWares/authMiddleWare');
-const { getAllDoctors, addDoctor, addDocPhoto, appointmentAdmin, appointmentCancel } = require('../controllers/adminController');
+const {  addDoctor, addDocPhoto, appointmentAdmin, appointmentCancel } = require('../controllers/adminController');
 
-router.get('/get-docs', protect, getAllDoctors);
+//router.get('/get-docs', protect, getAllDoctors);
 router.post('/add-doc', protect, adminOnly, addDoctor);
 router.post('/add-doc-photo', protect, adminOnly, addDocPhoto);
 router.get('/appointments', protect, adminOnly, appointmentAdmin);
@@ -15,36 +15,3 @@ module.exports = router;
 
 
 
-
-
-
-
-// const express = require('express')
-// const { loginAdmin, appointmentAdmin, appointmentCancel, addDoctor, addDocPhoto,  getAllDoctors } = require('../controllers/adminController')
-// const { protect, adminOnly } = require('../middleWares/authMiddleWare')
-// const router = express.Router()
-
-
-
-
-
-
-
-// //router.post('/admin-login', adminOnly, loginAdmin )
-//  router.get('/cancel', protect, adminOnly, appointmentCancel )
-//  router.get('/get-booking', protect, adminOnly, appointmentAdmin )
-//  router.post('/add-doc', protect, adminOnly, addDoctor)
-//  router.patch('/doc-photo', protect, adminOnly, addDocPhoto )
-
-//  router.get('/get-docs', protect,  getAllDoctors)
-
-
-
-
-
-
-
-
-
-
-// module.exports = router
