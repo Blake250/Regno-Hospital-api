606a929437a8d00e0155c8f5914db46ddcae1b33
Blake250 <ozoekwecelestine@gmail.com>
2025-07-31 09:27:03 +0100

    pushing fix

--- index.js
+++ index.js
@@ -27,10 +27,9 @@ app.use(cookieParser());
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'https://regno-hospital-app.vercel.app',
-      // 'http://localhost:5173',
-      // 'https://regno-hospital-app.netlify.app',
-      // 'https://regno-hospital-api.onrender.com',
+    
+       'https://regno-hospital-app.netlify.app',
+ 
     ];
    
    
